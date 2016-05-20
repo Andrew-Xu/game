5c01a34eddf03cd46cbc38d201105f42d39b47f0
Andrew-Xu <andrewxu0@gmail.com>
2016-05-19 23:04:23 -0700

    Create Literature.java

---
+++ Literature.java
@@ -0,0 +1,35 @@
+package subjects;
+
+import java.util.ArrayList;
+
+public class Literature 
+{	
+	double grade;
+
+	ArrayList<String> dilemma = new ArrayList<String>();//make the dilemmas per day in random order
+	ArrayList<String> choices = new ArrayList<String>();//maybe some choices can be unlocked by choosing previous ones
+	ArrayList<String> results = new ArrayList<String>();//results should change grades, influence the student, etc.
+
+	public Literature()
+	{
+		grade = 0;
+		dilemma.add("");
+		choices.add("");
+		results.add("");
+	}
+
+	public double getGrade() 
+	{
+		return grade;
+	}
+
+	public void setGrade(int newGrade) 
+	{
+		grade = newGrade;
+	}
+
+	public void choice() 
+	{
+		
+	}
+}
