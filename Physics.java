73cbf99ef823281e92ddcfa2dc0a7a2bd544411d
Andrew-Xu <andrewxu0@gmail.com>
2016-05-19 23:05:37 -0700

    Create Physics.java

---
+++ Physics.java
@@ -0,0 +1,36 @@
+package subjects;
+
+import java.util.ArrayList;
+
+public class Physics implements Subject
+{
+	double grade;
+	
+	ArrayList<String> dilemma = new ArrayList<String>();//make the dilemmas per day in random order
+	ArrayList<String> choices = new ArrayList<String>();//maybe some choices can be unlocked by choosing previous ones
+	ArrayList<String> results = new ArrayList<String>();//results should change grades, influence the student, etc.
+	
+	public Physics()
+	{
+		grade = 0;
+		dilemma.add("");
+		choices.add("");
+		results.add("");
+	}
+	
+	public double getGrade() 
+	{
+		return grade;
+	}
+
+	public void setGrade(int newGrade) 
+	{
+		grade = newGrade;
+	}
+
+	public void choice() 
+	{
+		
+	}
+	
+}
