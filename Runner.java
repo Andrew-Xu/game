600e0f775ada11b391cb1b3934c76a0ecbe05baf
TheKentrosaurus <kentaa98@gmail.com>
2016-05-25 20:26:53 -0700

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -10,28 +10,10 @@ public class Runner
 	private static GameWindow theGameWindow;
     public static void main( String[] args )
     {
-    	boolean check1 = false;
-    	boolean check2 = false;
-    	boolean check3 = false;
+
     	theGameWindow = new GameWindow();
     	theGameWindow.startIntroScreen();
-    	if (theGameWindow.isIntroOver())
-    	{
-    		theGameWindow.startTextGame();
-    	}
-    	if (theGameWindow.isTextGameOver())
-    	{
-    		
-    	}
-    	//theGameWindow.startMapGame();
         System.out.println("\nayy lmao");
         
-
     }
 }
-/*TurtleProgram tp = new TurtleProgram();
-tp.addAction( new TurnRight( 120 ) );
-tp.addAction( new MoveForward( 100 ) );
-tp.addAction( new MoveBack( 50 ) );
-tp.addAction( new TurnLeft( 90 ) );
-System.out.println( tp );*/
