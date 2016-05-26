96a492afd203ea5979d86e560f9a313846329ede
TheKentrosaurus <kentaa98@gmail.com>
2016-05-26 00:23:08 -0700

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -1,4 +1,3 @@
-
 package gui;
 
 import minigames.*;
@@ -14,6 +13,18 @@ public class Runner
     	theGameWindow = new GameWindow();
     	theGameWindow.startIntroScreen();
         System.out.println("\nayy lmao");
-        
+    	
+    	Map m = new Map(20, 10); //initialize floor
+    	Wall w = new Wall();
+    	w.initialize(m);
+		m.columnF(w.getLWall());
+		m.columnL(w.getRWall());
+		m.getMap()[0] = w.getCeiling();
+		m.getMap()[m.getY() - 1] = w.getFloor();
+		Player p = new Player(m.getY() - 2, 1);
+		m.getMap()[p.getX()][p.getY()] = p + "";
+		
+    	System.out.println("");
+    	System.out.println(m);
     }
 }
