6d0e75291d5f51cc2f5f40b740590635ade9f844
TheKentrosaurus <kentaa98@gmail.com>
2016-06-13 05:06:14 -0700

    Update and rename Subjects.java to Subject.java

---
+++ Subject.java
@@ -0,0 +1,8 @@
+package subjects;
+
+public interface Subject//interface is implemented with student designed classes
+{
+	public double getGrade();
+	public int setGrade(double g);
+	public void choice();
+}
--- Subjects.java
+++
@@ -1,8 +0,0 @@
-package subjects;
-
-public interface Subject
-{
-	public double getGrade();
-	public void setGrade(int g);
-	public void choice();
-}
